0ed27459fcd4ae654528759cb441138242758b66
Noctsol <39346808+Noctsol@users.noreply.github.com>
2018-07-30 11:47:02 -0700

    Add project files.

---
+++ Tests/ExampleCode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Tests
+{
+  class ExampleCode
+  {
+    static void Main(string[] args)
+		 { //print function cause console.writeline is fucking aids
+      void print(object arg)
+      {
+        Console.WriteLine(arg);
+        //Console.ReadLine(); only use if outputting to cmd/console window
+      }
+      //Dictionary type
+      Dictionary<string, string> thing = new Dictionary<string, string>
+        {["name"] = "Malphie", ["weight"] = "1", ["color"] = "brown"};
+
+      foreach (KeyValuePair<string, string> attr in thing){ 
+        print(attr.Key + " " + attr.Value + " " + attr); } 
+       print(thing["name"]);
+      
+      // Arrays and lists
+      List<string> lst = new List<string> {"item1","item2","item3" };
+      foreach (string i in lst){
+        print(i);
+      }
+      // Array with ints and strings - not good practice
+
+      
+
+      
+    }
+  }
+}
